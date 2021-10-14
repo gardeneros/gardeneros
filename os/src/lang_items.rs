57e4f91fbf5415f42ae3f7a1004fe7ddf805e854
Di Zhang <dizhang@bjtu.edu.cn>
2021-10-14 13:44:29 +0800

    Min kernel ok

---
+++ os/src/lang_items.rs
@@ -0,0 +1,16 @@
+use crate::sbi::shutdown;
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
+}
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,71 +1,44 @@
 #![no_std]
 #![no_main]
 #![feature(asm)]
-
-use core::panic::PanicInfo;
-use core::fmt::{self, Write};
-
-
-const SYSCALL_EXIT: usize = 93;
-const SYSCALL_WRITE: usize = 64;
-
-struct Stdout;
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
-        Ok(())
+#![feature(global_asm)]
+#![feature(panic_info_message)]
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+global_asm!(include_str!("entry.asm"));
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
     }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
-
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
-
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
-    unsafe {
-        asm!("ecall",
-             in("x10") args[0],
-             in("x11") args[1],
-             in("x12") args[2],
-             in("x17") id,
-             lateout("x10") ret
-        );
-    }
-    ret
-}
-
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
-
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
-
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
 #[no_mangle]
-extern "C" fn _start() {
-    //loop{};
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    clear_bss();
     println!("Hello, world!");
-    sys_exit(9);
+    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    println!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
 }
