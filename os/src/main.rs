140b89de5f101528544139b271ec2eeec6f7303d
Di Zhang <dizhang@bjtu.edu.cn>
2021-10-19 10:29:50 +0800

    Complete batch os

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,13 +1,21 @@
 #![no_std]
 #![no_main]
 #![feature(asm)]
+
 #![feature(global_asm)]
 #![feature(panic_info_message)]
+
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
+mod syscall;
+mod trap;
+mod batch;
+
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
 fn clear_bss() {
     extern "C" {
         fn sbss();
@@ -15,30 +23,12 @@ fn clear_bss() {
     }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
+
 #[no_mangle]
 pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext();
-        fn etext();
-        fn srodata();
-        fn erodata();
-        fn sdata();
-        fn edata();
-        fn sbss();
-        fn ebss();
-        fn boot_stack();
-        fn boot_stack_top();
-    }
     clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
-        "boot_stack [{:#x}, {:#x})",
-        boot_stack as usize, boot_stack_top as usize
-    );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    println!("Hello, world!");
-    panic!("Shutdown machine!");
+    println!("[Kernel] Hello, world!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
---
+++ os/src/trap/mod.rs
@@ -0,0 +1,51 @@
+mod context;
+
+use riscv::register::{
+    mtvec::TrapMode,
+    stvec,
+    scause::{
+        self,
+        Trap,
+        Exception,
+    },
+    stval,
+};
+
+use crate::syscall::syscall;
+use crate::batch::run_next_app;
+
+global_asm!(include_str!("trap.S"));
+
+pub fn init() {
+    extern "C" { fn __alltraps(); }
+    unsafe {
+        stvec::write(__alltraps as usize, TrapMode::Direct);
+    }
+}
+
+#[no_mangle]
+pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+    let scause = scause::read();
+    let stval = stval::read();
+    match scause.cause() {
+        Trap::Exception(Exception::UserEnvCall) => {
+            cx.sepc += 4;
+            cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
+        }
+        Trap::Exception(Exception::StoreFault) |
+        Trap::Exception(Exception::StorePageFault) => {
+            println!("[kernel] PageFault in application, core dumped.");
+            run_next_app();
+        }
+        Trap::Exception(Exception::IllegalInstruction) => {
+            println!("[kernel] IllegalInstruction in application, core dumped.");
+            run_next_app();
+        }
+        _ => {
+            panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
+        }
+    }
+    cx
+}
+
+pub use context::TrapContext;
