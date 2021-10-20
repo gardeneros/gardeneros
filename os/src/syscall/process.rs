b1b00cc432b9f111447bfafce060282354a85c87
Di Zhang <dizhang@bjtu.edu.cn>
2021-10-20 23:17:36 +0800

    Minor

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -3,6 +3,8 @@ use crate::task::{
     exit_current_and_run_next,
 };
 
+use crate::timer::get_time_ms;
+
 pub fn sys_exit(exit_code: i32) -> ! {
     println!("[kernel] Application exited with code {}", exit_code);
     exit_current_and_run_next();
@@ -12,4 +14,8 @@ pub fn sys_exit(exit_code: i32) -> ! {
 pub fn sys_yield() -> isize {
     suspend_current_and_run_next();
     0
+}
+
+pub fn sys_get_time() -> isize {
+    get_time_ms() as isize
 }
