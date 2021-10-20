2fc407331c00ec80a5b43be07483bc7658f8e63a
Di Zhang <dizhang@bjtu.edu.cn>
2021-10-20 16:42:19 +0800

    Implement timesharing os

--- os/src/main.rs
+++ os/src/main.rs
@@ -13,6 +13,7 @@ mod trap;
 mod loader;
 mod config;
 mod task;
+mod timer;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -31,6 +32,8 @@ pub fn rust_main() -> ! {
     println!("[Kernel] Hello, world!");
     trap::init();
     loader::load_apps();
+    trap::enable_timer_interrupt();
+    timer::set_next_trigger();
     task::run_first_task();
     panic!("Unreachable in rust_main!");
 }
--- user/src/lib.rs
+++ user/src/lib.rs
@@ -38,4 +38,5 @@ use syscall::*;
 pub fn write(fd: usize, buf: &[u8]) -> isize { sys_write(fd, buf) }
 pub fn exit(exit_code: i32) -> isize { sys_exit(exit_code) }
 pub fn yield_() -> isize { sys_yield() }
+pub fn get_time() -> isize { sys_get_time() }
 
