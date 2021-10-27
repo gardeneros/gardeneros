22873bb352900dfac92faecabc2843ce456ed55b
Di Zhang <dizhang@bjtu.edu.cn>
2021-10-27 17:01:45 +0800

    Implement frame management

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,6 +1,14 @@
 mod heap_allocator;
+mod address;
+mod frame_allocator;
+
+pub use address::{PhysAddr, VirtAddr, PhysPageNum, VirtPageNum, StepByOne};
+pub use frame_allocator::{FrameTracker, frame_alloc, frame_dealloc,};
+
 
 pub fn init() {
 	heap_allocator::init_heap();
 	heap_allocator::heap_test();
+    frame_allocator::init_frame_allocator();
+    frame_allocator::frame_allocator_test();
 }
