d4a826b785a5409862358b6ca7e504c50d7093fd
Di Zhang <dizhang@bjtu.edu.cn>
2022-10-24 16:43:45 +0800

    Minor asm and global_asm

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,12 +1,14 @@
 #![no_std]
 #![no_main]
-#![feature(asm)]
-#![feature(global_asm)]
 #![feature(panic_info_message)]
 #[macro_use]
+
 mod console;
 mod lang_items;
 mod sbi;
+
+use core::arch::global_asm;
+
 global_asm!(include_str!("entry.asm"));
 fn clear_bss() {
     extern "C" {
@@ -15,6 +17,7 @@ fn clear_bss() {
     }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
+
 #[no_mangle]
 pub fn rust_main() -> ! {
     extern "C" {
